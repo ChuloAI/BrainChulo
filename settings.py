41e50748319ed3f0ff94a05c884060d2621ef36c
Gav <info@cryptorush.app>
2023-04-22 17:50:15 -0400

    introduce settings

---
+++ settings.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings():
+  def __init__(self):
+    # Chat API - By default, we are assuming Oobabooga's Text Generation WebUI is running
+    self.chat_api_host = os.getenv("CHAT_API_HOST", "http://localhost")
+    self.chat_api_port = os.getenv("CHAT_API_PORT", 7860)
+    self.chat_api_path = os.getenv("CHAT_API_PATH", "/run/textgen/")
+
+    # Where short-term memory is stored
+    self.index_path = os.getenv("INDEX_PATH", "index.json")
+
+    # Default objective - If we go objective-based, this is the default
+    self.default_objective = os.getenv("DEFAULT_OBJECTIVE", "Be a CEO.")
+
+
+def load_config():
+  return Settings()
