2bd04a8e38646972dca92ecaddfbd4db0bc9eab9
Gav <info@cryptorush.app>
2023-04-22 17:52:36 -0400

    Conditional Index Persistence

--- main.py
+++ main.py
@@ -20,9 +20,16 @@ llm_predictor = LLMPredictor(llm=VicunaLLM())
 service_context = ServiceContext.from_defaults(
     llm_predictor=llm_predictor, prompt_helper=prompt_helper)
 
-documents = SimpleDirectoryReader('data').load_data()
-index = GPTListIndex.from_documents(documents, service_context=service_context)
-index.save_to_disk('index.json')
+# Create or load index
+# Check if index.json exists
+if os.path.exists("index.json"):
+  index = GPTListIndex.load_from_disk(
+    "index.json", service_context=service_context)
+else:
+  documents = SimpleDirectoryReader('data').load_data()
+  index = GPTListIndex.from_documents(
+    documents, service_context=service_context)
+  index.save_to_disk('index.json')
 
 response = index.query("What did the author do growing up?")
 print(response)
