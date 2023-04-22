afa8557f4d58038b0d0a88f62efd841f0e094395
Gav <info@cryptorush.app>
2023-04-22 17:55:20 -0400

    use index_path from settings

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
-from dotenv import load_dotenv
 from llama_index import GPTListIndex, SimpleDirectoryReader, PromptHelper, QuestionAnswerPrompt
 from llama_index import LLMPredictor, ServiceContext
 from vicuna_llm import VicunaLLM
+import settings
 
-load_dotenv()
+config = settings.load_config()
 
 # define prompt helper
 # set maximum input size
@@ -23,14 +23,14 @@ service_context = ServiceContext.from_defaults(
 
 # Create or load index
 # Check if index.json exists
-if os.path.exists("index.json"):
+if os.path.exists(config.index_path):
   index = GPTListIndex.load_from_disk(
     "index.json", service_context=service_context)
 else:
   documents = SimpleDirectoryReader('data').load_data()
   index = GPTListIndex.from_documents(
     documents, service_context=service_context)
-  index.save_to_disk('index.json')
+  index.save_to_disk(config.index_path)
 
 # define custom QuestionAnswerPrompt
 query_str = "What did the author do growing up?"
