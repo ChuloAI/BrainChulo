5aed288daa4c015a322b5db620796c291753fd80
Gav <info@cryptorush.app>
2023-04-27 08:34:16 -0400

    use new huggingface embeddings library

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from langchain.document_loaders import TextLoader
 from langchain.vectorstores import Chroma
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains import ConversationalRetrievalChain
-from embeddings.chroma_mini_llm import ChromaMiniLM
+from langchain.embeddings import HuggingFaceEmbeddings
 from vicuna_llm import VicunaLLM
 import settings
 
@@ -11,11 +11,12 @@ config = settings.load_config()
 loader = TextLoader('./data/paul_graham_essay.txt', encoding="utf8")
 documents = loader.load()
 
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 documents = text_splitter.split_documents(documents)
 
-embeddings = ChromaMiniLM()
-vectorstore = Chroma.from_documents(documents, embeddings)
+embeddings = HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
+vectorstore = Chroma.from_documents(documents, embeddings, metadatas=[
+    {"source": str(i)} for i in range(len(documents))])
 
 # define custom QuestionAnswerPrompt
 query_str = "Where did the author grow up?"
