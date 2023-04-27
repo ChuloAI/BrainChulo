a6f6ea40dfef4e45cf60a25720e444c63ea4270a
Gav <info@cryptorush.app>
2023-04-27 15:50:43 -0400

    better document-based memory

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from langchain.document_loaders import TextLoader
 from langchain.vectorstores import Chroma
+from langchain.memory import VectorStoreRetrieverMemory
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.chains import ConversationalRetrievalChain
+from langchain.chains import ConversationChain
 from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.prompts import PromptTemplate
 from vicuna_llm import VicunaLLM
+from prompt_templates.friendly_conversation import Template
 import settings
 
 config = settings.load_config()
@@ -17,19 +20,27 @@ documents = text_splitter.split_documents(documents)
 embeddings = HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
 vectorstore = Chroma.from_documents(documents, embeddings, metadatas=[
     {"source": str(i)} for i in range(len(documents))])
+retriever = vectorstore.as_retriever(search_kwargs=dict(k=20))
+memory = VectorStoreRetrieverMemory(retriever=retriever)
+
+
+PROMPT = PromptTemplate(
+    input_variables=["history", "input"], template=Template
+)
 
 # define custom QuestionAnswerPrompt
 query_str = "Where did the author grow up?"
 
-qna = ConversationalRetrievalChain.from_llm(
-    VicunaLLM(), vectorstore.as_retriever())
+convo = ConversationChain(
+    llm=VicunaLLM(),
+    prompt=PROMPT,
+    memory=memory,
+    verbose=False
+)
 
-chat_history = []
-response = qna({"question": query_str, "chat_history": chat_history})
 
-chat_history.append((query_str, response['answer']))
+response = convo.predict(input=query_str)
+print(f"Response from AI: {response}")
 
-response = qna({"question": 'Who is the author in love with?',
-               "chat_history": chat_history})
-
-print(response)
+response = convo.predict(input="Who did the author marry?")
+print(f"Response from AI: {response}")
