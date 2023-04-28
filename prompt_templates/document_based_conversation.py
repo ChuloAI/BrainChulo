48d2ccd5c3bc2f8a318eba5184afbf41ef77d428
Gav <info@cryptorush.app>
2023-04-28 17:39:32 -0400

    document-based conversation base template - WIP

---
+++ prompt_templates/document_based_conversation.py
@@ -0,0 +1,10 @@
+Template = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+
+Relevant pieces of previous conversation:
+{history}
+
+(You do not need to use these pieces of information if not relevant)
+
+Current conversation:
+Human: {input}
+AI:"""
