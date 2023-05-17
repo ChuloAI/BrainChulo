ab4156e1890af0469268430bf44e7ae4285bc5b5
Gavroche <info@cryptorush.app>
2023-05-16 20:52:52 -0400

    fix bubbles

--- frontend/src/components/ChatBubble.vue
+++ frontend/src/components/ChatBubble.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex items-end mb-4">
-    <div class="flex items-center justify-center w-10 h-10 rounded-full bg-gray-400 text-white mr-2">
-      <img :src="avatarUrl" alt="Avatar" class="w-full h-full object-cover rounded-full" />
+  <div class="flex items-start mb-4 w-full gap-2">
+    <div class="flex items-center justify-center w-10 h-10 rounded-full bg-gray-400 text-white flex-shrink-0 ml-4">
+      <img :src="avatarUrl" alt="Avatar" class="object-cover rounded-full w-full h-full" />
     </div>
-    <div class="rounded-lg py-2 px-4">
-      <div class="text-gray-900 text-md">{{ message.content }}</div>
+    <div class="rounded-lg py-2 mx-5">
+      <div class="text-gray-900 text-md max-w-xl">{{ message.text }}</div>
     </div>
   </div>
   <hr class="w-full mb-4" />
@@ -20,7 +20,7 @@
     },
     computed: {
       avatarUrl() {
-        const path = this.message.isMe ? '../assets/user_icon.png' : '../assets/AI_icon.png';
+        const path = this.message.is_user ? '../assets/user_icon.png' : '../assets/AI_icon.png';
 
         return new URL(path, import.meta.url).href;
       },
