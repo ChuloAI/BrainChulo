9859f7dce7eb27ee36d10a59210cd516d542f95a
Gavroche <info@cryptorush.app>
2023-05-16 20:53:20 -0400

    persist message api

--- frontend/src/services/internal.js
+++ frontend/src/services/internal.js
@@ -5,18 +5,32 @@ class InternalService {
         this.baseUrl = config.baseUrl;
     }
 
+    /* Get the current conversation or create a new one */
     async getConversation(id) {
         if(id) {
-            return this.request(`/conversations/${id}`, "GET");
+            return await this.request(`/conversations/${id}`, "GET");
         } else {
             // POST to /conversations to create a new conversation
-            return this.request('/conversations', 'POST')
+            return await this.request('/conversations', 'POST')
         }
     }
 
+    /* Send a message to a conversation */
+    async sendMessage(conversationId, message) {
+        console.log(message)
+        return await this.request(`/conversations/${conversationId}/messages`, 'POST', message);
+    }
+
     async request(url, method, payload = {}, headers = {}, params = { }) {
+        if(method.toLowerCase() == "get") payload = undefined;
+        else {
+            payload = JSON.stringify(payload);
+        }
+        
         const defaultHeaders = {
             'Access-Control-Request-Method': 'GET, DELETE, HEAD, POST, OPTIONS',
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
         };
 
         let fullPath = `${this.baseUrl}${url}`;
