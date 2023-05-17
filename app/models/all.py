4cf811a8f64108a29b6781d4e6051522f1215991
Gavroche <info@cryptorush.app>
2023-05-16 20:52:38 -0400

    update message schema

--- app/models/all.py
+++ app/models/all.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Optional, List
 from sqlmodel import SQLModel, Field, Relationship
 
@@ -9,6 +10,7 @@ class Message(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     text: str
     is_user: bool
-    conversation_id: Optional[int] = Field(default=None, foreign_key="conversation.id")
-    conversation: Optional["Conversation"] = Relationship(back_populates="messages")
+    conversation_id: int = Field(default=None, foreign_key="conversation.id")
+    conversation: Conversation = Relationship(back_populates="messages")
+    created_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
 
