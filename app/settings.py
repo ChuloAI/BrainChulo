fab4ed434c57518909b9ef4f7492586aac009929
Gavroche <info@cryptorush.app>
2023-05-17 09:45:59 -0400

    revert instruct model update

--- app/settings.py
+++ app/settings.py
@@ -5,20 +5,25 @@ import logging
 load_dotenv()
 
 logging.basicConfig(
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    level=logging.INFO)
+    format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO
+)
 logger = logging.getLogger(__name__)
 
 
-class Settings():
+class Settings:
     def __init__(self):
         # Chat API - By default, we are assuming Oobabooga's Text Generation
         # WebUI is running
         self.embeddings_model = os.getenv(
-            "EMBEDDINGS_MODEL", "hkunlp/instructor-xl")
-        
+            "EMBEDDINGS_MODEL", "all-MiniLM-L6-v2"
+        )
+
         self.chat_api_url = os.getenv(
-            "CHAT_API_URL", "http://localhost:5000/api/v1/generate")
+            "CHAT_API_URL", "http://localhost:5000/api/v1/generate"
+        )
+
+        # Where all data is stored
+        self.data_path = os.getenv("DATA_PATH", f"{os.getcwd()}/data/")
 
         # Where short-term memory is stored
         self.memories_path = os.getenv("MEMORIES_PATH", "memories/")
@@ -26,11 +31,18 @@ class Settings():
         # Where uploads are saved
         self.upload_path = os.getenv("UPLOAD_PATH", "uploads/")
 
+        # Where conversation history is stored
+        self.conversation_history_path = os.getenv(
+            "CONVERSATION_HISTORY_PATH", "conversation_history/"
+        )
+
         # Document store name
         self.document_store_name = os.getenv(
-            "DOCUMENT_STORE_NAME", "brainchulo_docs")
+            "DOCUMENT_STORE_NAME", "brainchulo_docs"
+        )
         self.conversation_store_name = os.getenv(
-            "CONVERSATION_STORE_NAME", "brainchulo_convos")
+            "CONVERSATION_STORE_NAME", "brainchulo_convos"
+        )
 
         # Default objective - If we go objective-based, this is the default
         self.default_objective = os.getenv("DEFAULT_OBJECTIVE", "Be a CEO.")
