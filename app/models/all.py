646d87d7d312d4c0d0e2d341c724c78f983ec7b9
Gavroche <info@cryptorush.app>
2023-05-22 10:46:41 -0400

    ensure we return conversations with their messages

--- app/models/all.py
+++ app/models/all.py
@@ -2,17 +2,30 @@ from datetime import datetime
 from typing import Optional, List
 from sqlmodel import SQLModel, Field, Relationship
 
-class Conversation(SQLModel, table=True):
+class ConversationBase(SQLModel):
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    title: Optional[str]
+
+class Conversation(ConversationBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     messages: List["Message"] = Relationship(back_populates="conversation")
 
-class Message(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+class ConversationRead(ConversationBase):
+    id: int
+
+class MessageBase(SQLModel):
     text: str
     is_user: bool
     conversation_id: Optional[int] = Field(default=None, foreign_key="conversation.id")
-    conversation: Optional[Conversation] = Relationship(back_populates="messages")
     rating: int = 0 # -1, 0, 1
     created_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
 
+class Message(MessageBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    conversation: Optional[Conversation] = Relationship(back_populates="messages")
 
+class MessageRead(MessageBase):
+    id: int
+
+class ConversationWithMessages(ConversationRead):
+    messages: List[MessageRead] = []
