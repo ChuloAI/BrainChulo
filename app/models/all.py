35eedb2d522745a060c748f9876cd4e16c9e6eda
Gavroche <info@cryptorush.app>
2023-05-22 10:13:24 -0400

    add rating

--- app/models/all.py
+++ app/models/all.py
@@ -10,7 +10,9 @@ class Message(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     text: str
     is_user: bool
-    conversation_id: int = Field(default=None, foreign_key="conversation.id")
-    conversation: Conversation = Relationship(back_populates="messages")
+    conversation_id: Optional[int] = Field(default=None, foreign_key="conversation.id")
+    conversation: Optional[Conversation] = Relationship(back_populates="messages")
+    rating: int = 0 # -1, 0, 1
     created_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
+
 
