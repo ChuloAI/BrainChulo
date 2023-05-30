97f888a064562dd21b1f3275df3a6d5058400a37
Paolo Rechia <paolorechia@gmail.com>
2023-05-30 21:51:53 +0200

    Remove remaining fiels

--- guidance_agent/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-trap 'kill $(jobs -p)' EXIT
-
-cd /home/karajan/labzone/gd_tests/langchain_guidance/frontend && npm run serve
-conda activa txtgdc2
-cd /home/karajan/labzone/gd_tests/langchain_guidance && python gdc_server.py&
