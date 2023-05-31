a9c8e4fc5b9585eea5cd10061fe394bae5af91c4
Paolo Rechia <paolorechia@gmail.com>
2023-05-31 11:28:43 +0200

    Adds accelerate and bitsandbytes to requirements

--- app/model_singleton.py
+++ app/model_singleton.py
@@ -9,12 +9,5 @@ llama = None
 
 def load_model_into_guidance(settings: Settings):
     global llama
-    import os
-
-    print("Wut")
-    print(os.getcwd())
-    print(os.listdir("/models"))
-    print(os.listdir("/models/wizardlm"))
-    print("What")
     llama = guidance.llms.Transformers(model=settings.model_path, device_map="auto", load_in_8bit=True)
     return llama
