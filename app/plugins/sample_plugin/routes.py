9eb2aeea2bb3cbd7df0cc26e86090f8dc35ee180
Gavroche <info@cryptorush.app>
2023-06-04 19:10:22 -0400

    sample plugin route

---
+++ app/plugins/sample_plugin/routes.py
@@ -0,0 +1,11 @@
+# plugins/sample_plugin/routes.py
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get("/")
+def sample_plugin_route():
+    return {"message": "Sample plugin route"}
+
+# Additional routes and functionality for the sample plugin
