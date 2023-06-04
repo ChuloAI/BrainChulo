09ac8e3824eeb4c36aa5cd342cb78c46ec90d308
Gavroche <info@cryptorush.app>
2023-06-04 19:10:33 -0400

    sample plugin database

---
+++ app/plugins/sample_plugin/database.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from typing import Optional, List
+from sqlmodel import SQLModel, Field, Relationship
+from models.all import Conversation
+
+metadata = SQLModel.metadata
+
+class SamplePluginModelBase(SQLModel):
+    """A base model for SamplePluginModel"""
+    # __tablename__: str = 'sample_plugin_model'
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    title: Optional[str]
+
+class SamplePluginModel(SamplePluginModelBase, table=True):
+    """A model for SamplePlugin"""
+    id: Optional[int] = Field(default=None, primary_key=True)
+    conversations: List["Conversation"] = Relationship(back_populates="SamplePluginModel")
+
+class SamplePluginModelRead(SamplePluginModelBase):
+    """A read model for SamplePlugin"""
+    id: int
