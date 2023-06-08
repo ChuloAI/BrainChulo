f4d0ae8c650da0320abb5876d9fa842681287553
Gavroche <info@cryptorush.app>
2023-06-08 17:13:20 -0400

    Temporarily disregard origins

--- app/main.py
+++ app/main.py
@@ -55,7 +55,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[*origins],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
