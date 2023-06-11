551ba0a129844df938eb7c1c09d8c3c1319b6174
karajan <hugothomel@gmail.com>
2023-06-11 16:45:53 +0200

    restored the previous agent as ChainOfThoughtsFlowAgent

--- app/agents/chain_of_thoughts.py
+++ app/agents/chain_of_thoughts.py
@@ -1,6 +1,6 @@
 from typing import Dict, Callable
 from andromeda_chain import AndromedaChain
-from agents.flow_based import BaseFlowAgent
+from agents.flow_based import BaseAgent, BaseFlowAgent
 from flow.flow import Flow, PromptNode, ToolNode, ChoiceNode, StartNode
 from prompts.flow_guidance_cot import FlowChainOfThoughts, PROMPT_START_STRING
 from guidance_tooling.guidance_programs.tools import ingest_file
@@ -26,7 +26,7 @@ def get_llm():
     if llm is None:
         print("Loading guidance model...")
         model_type = "LlamaCpp"
-        model_path ="/home/karajan/Downloads/open-llama-3b-q8_0.bin"
+        model_path ="/home/karajan/Downloads/airoboros-13b-gpt4.ggmlv3.q8_0.bin"
         model_n_ctx =1000
         target_source_chunks = 4
         n_gpu_layers = 500
@@ -37,9 +37,47 @@ def get_llm():
         llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, callbacks=callbacks, verbose=False,n_gpu_layers=n_gpu_layers, use_mlock=use_mlock,top_p=0.9, n_batch=n_batch)
     return llm
 
+class ChainOfThoughtsFlowAgent(BaseFlowAgent):
+    def __init__(self, andromeda: AndromedaChain, tools: Dict[str, Callable[[str], str]]):
+        def execute_tool(variables):
+            action_name = variables["tool_name"]
+            action_input = variables["act_input"]
+            return self.do_tool(action_name, action_input)
+
+        start = StartNode("start", FlowChainOfThoughts.flow_prompt_start, {
+            "Action": "choose_action",
+            "Final Answer": "final_prompt"
+        })
+        thought = PromptNode("thought", FlowChainOfThoughts.thought_gen)
+        choose_action = PromptNode("choose_action", FlowChainOfThoughts.choose_action)
+        perform_action = PromptNode("perform_action", FlowChainOfThoughts.action_input)
+        execute_tool_node = ToolNode("execute_tool", execute_tool)
+        decide = ChoiceNode("decide", ["thought", "final_prompt"], max_decisions=3, force_exit_on="final_prompt")
+        final = PromptNode("final_prompt", FlowChainOfThoughts.final_prompt)
+
+        thought.set_next(choose_action)
+        choose_action.set_next(perform_action)
+        perform_action.set_next(execute_tool_node)
+        execute_tool_node.set_next(decide)
+
+        flow = Flow(
+            [start, thought, choose_action, perform_action, execute_tool_node, decide, final]
+        )
+
+        super().__init__(andromeda, tools, flow)
+        self.valid_tools = list(tools.keys())
+        self.valid_answers = ["Action", "Final Answer"]
 
 
-class ChainOfThoughtsAgent(BaseFlowAgent):
+    def run(self, query: str) -> str:
+        return super().run(query, variables={
+            "prompt_start": PROMPT_START_STRING,
+            "question": query,
+            "valid_tools": self.valid_tools,
+            "valid_answers": self.valid_answers,
+        })
+
+class ChainOfThoughtsAgent(BaseAgent):
     def __init__(self, guidance, retriever, num_iter=3):
         self.guidance = guidance
         self.retriever = ingest_file(TEST_FILE)
@@ -72,7 +110,7 @@ class ChainOfThoughtsAgent(BaseFlowAgent):
         Observation: Based on the documents, I think I can reach a conclusion.
         {{#if (can_answer)}} 
         Thought: I believe I can answer the question based on the information contained in the returned documents.
-        Final Answer: {{gen 'answer' temperature=0.7 max_tokens=500}}
+        Final Answer: {{gen 'answer' temperature=0.7 max_tokens=50}}
         {{else}}
         Thought: I don't think I can answer the question based on the information contained in the returned documents.
         Final Answer: I'm sorry, but I don't have sufficient information to provide an answer to this question.
