90284d12838d630bdc62588e35e5f0eba64b7965
Hugo <hugo@world.game>
2023-06-16 15:32:30 +0200

    Brought prompt without ethics check up to par

--- app/prompt_templates/qa_agent.py
+++ app/prompt_templates/qa_agent.py
@@ -37,6 +37,7 @@ Decision:{{#select 'query_type' logprobs='logprobs'}}Phatic{{or}}Referential{{/s
 Observation: The user's query is conversational. I need to answer him as an helpful assistant while taking into account our chat history;
 Chat history: {{history}}
 Latest user message: {{question}}
+Thought: I need to stay in my role of an helpful assistant and make casual conversation.
 Final Answer: {{gen 'phatic answer' temperature=0.7 max_tokens=50}}
 {{else}}
 
@@ -87,47 +88,46 @@ Action: Check Question
 Action Input: {{question}}
 {{~/assistant}}
 
+{{~! The assistant then classifies the user intent to decide whether he needs to enter qa mode}}
 {{#user~}}
-Here are the relevant documents from our database:{{search question}}
-Given the documents listed, can you determine an answer to the following question based solely on the provided information: {{question}} Note that your response MUST contain either 'yes' or 'no'.
+Question: {{question}}
+Now classify the intent behind this question:{{question}} Identify if this question is phatic (serving a social function) or referential (seeking information). Provide reasoning for your classification based on elements like the content, structure, or context of the user's input.
 {{~/user}}
 
 {{#assistant~}}
-Observation: I need to determine if I can answer the question based solely on the returned documents.
-Thought: Inferential analysis of {{question}} for potential answers, telegraphic style.
-Summary {{gen 'summary' temperature=0.5 max_tokens=120}}
+Thought: I need to evaluate the user's query and determine its intent - is {{question}} phatic or referential?
+Decision:{{#select 'query_type' logprobs='logprobs'}}Phatic{{or}}Referential{{/select}}
 {{~/assistant}}
 
+{{#if (equal query_type "Phatic")~}}
+Observation: The user's query is conversational. I need to answer him as an helpful assistant while taking into account our chat history;
+Chat history: {{history}}
+Latest user message: {{question}}
+Thought: I need to stay in my role of an helpful assistant and make casual conversation.
+Final Answer: {{gen 'phatic answer' temperature=0.7 max_tokens=50}}
+{{else}}
+
 {{#user~}}
-Given your analysis, can you determine an answer to the following question based solely on the provided information: {{question}} Consider all details, even if they seem only remotely related or are uncertain.
+Here are the relevant documents from our database:{{set 'documents' (search question)}}
+Using the concept of deixis, please evaluate if the answer to {{question}} is in :{{documents}}. Note that your response MUST contain either 'yes' or 'no'.
 {{~/user}}
 
 {{#assistant~}}
-Thought: I need to determine if I can answer {{question}} based solely on the information provided in my analysis. 
-Decision:{{#select 'answer' logprobs='logprobs'}}Yes{{or}}No{{/select}}
-{{#if (equal answer "Yes")~}}
-Observation: I believe I can answer {{question}} based on the information contained in my analysis. 
+Thought: I need to determine if the answer to {{question}} is in: {{documents}}.
+Decision:{{#select 'answerable' logprobs='logprobs'}}Yes{{or}}No{{/select}}
+
+{{#if (equal answerable "Yes")~}}
+Observation: I believe I can answer {{question}} based on the information contained in my analysis.
+Thought: Now that I know that I can answer, I should provide the information to the user.
 Final Answer: {{gen 'answer' temperature=0 max_tokens=100}}
 {{else}}
 Thought: I don't think I can answer the question based on the information contained in the returned documents.
 Final Answer: I'm sorry, but I don't have sufficient information to provide an answer to this question.
 
-
-{{#assistant~}}
-Observation: I need to determine if I can answer the question based solely on the returned documents.
-Thought: Let's first gen a bullet points summary of the returned documents trying to grasp the most relevant elements to the question.
-Summary {{gen 'summary' temperature=0 max_tokens=100}}
-Decision:{{#select 'answer' logprobs='logprobs'}}Yes{{or}}No{{/select}}
-
-{{#if (equal answer "Yes")~}}
-Thought: I believe I can answer {{question}} based on the information contained in the summary. 
-Final Answer: {{gen 'final answer' temperature=0.7 max_tokens=50}}
-{{else}}
-Thought: I don't think I can answer the question based on the information contained in the returned documents.
-Final Answer: I'm sorry, but I don't have sufficient information to provide an answer to this question.
 {{/if}}
-
 {{~/assistant}}
+
+{{/if}}
 
 """
 ETHICS_PROMPT= '''
