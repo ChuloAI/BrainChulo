2afe3a9a4460fae67a2b55e4eedc81c9f7fddb93
Gavroche <info@cryptorush.app>
2023-06-25 16:37:53 -0400

    start work on about view

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <NavBar :username="username" :avatar-url="avatarUrl" @update-profile="updateProfile" @clear-messages="clearMessages"></NavBar>
+
+    <h1>About View</h1>
+    <p>This is the about view.</p>
   </div>
 </template>
 
+<script>
+  import NavBar from '@/components/NavBar.vue';
+
+  export default {
+    components: {
+      NavBar,
+    },
+  };
+</script>
+
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
   }
-}
 </style>
