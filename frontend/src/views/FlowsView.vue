24652cd452fd068302418d9c09fb1b95ece4487e
Gavroche <info@cryptorush.app>
2023-07-12 18:48:20 +0200

    add LLM Query Node to Flows

--- frontend/src/views/FlowsView.vue
+++ frontend/src/views/FlowsView.vue
@@ -1,14 +1,31 @@
 <script setup>
   import '@/assets/flows.css';
+  import { markRaw, onMounted } from 'vue';
+
   import NavBar from '@/components/NavBar.vue';
   import { Panel, VueFlow, useVueFlow } from '@vue-flow/core';
   import { Controls } from '@vue-flow/controls';
   import { Background, BackgroundVariant } from '@vue-flow/background';
   import { MiniMap } from '@vue-flow/minimap';
+  import LLMQueryNode from '@/nodes/LLMQueryNode.vue';
 
   const { nodes, addNodes, addEdges, onConnect, dimensions } = useVueFlow();
 
+  const nodeTypes = { 'llm-query': markRaw(LLMQueryNode) };
+
   onConnect((params) => addEdges(params));
+
+  onMounted(() => {
+    // Add an element after mount
+    addNodes([
+      {
+        id: '1',
+        type: 'llm-query',
+        position: { x: 1050, y: 500 },
+        label: 'LLM Query Node 1',
+      },
+    ]);
+  });
 
   function addRandomNode() {
     const nodeId = (nodes.value.length + 1).toString();
@@ -16,6 +33,7 @@
     const newNode = {
       id: nodeId,
       label: `Node: ${nodeId}`,
+      type: 'llm-query',
       position: { x: Math.random() * dimensions.value.width, y: Math.random() * dimensions.value.height },
     };
 
@@ -27,7 +45,7 @@
   <main class="h-screen">
     <NavBar></NavBar>
 
-    <VueFlow>
+    <VueFlow :node-types="nodeTypes">
       <Background :variant="BackgroundVariant.Lines" />
 
       <Panel position="top-right">
