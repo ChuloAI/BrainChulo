cdfeb00da62178804895f8013f984a96e00df31d
Gavroche <info@cryptorush.app>
2023-07-12 14:14:26 +0200

    add flows to navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -18,6 +18,15 @@
               </li>
               <li>
                 <router-link
+                  to="/flows"
+                  class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  exact-active-class="bg-gray-900 text-white"
+                  active-class="bg-gray-700 text-white">
+                  Flows
+                </router-link>
+              </li>
+              <li>
+                <router-link
                   to="/about"
                   class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   exact-active-class="bg-gray-900 text-white"
