195c3cf1a3bd20f1877f168dae850d739a6c4847
Gavroche <info@cryptorush.app>
2023-07-13 21:15:34 +0200

    WIP - Text Node

---
+++ frontend/src/nodes/TextNode.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-center">
+    <h1>Text Output</h1>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['text'],
+  };
+</script>
