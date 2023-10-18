0b3776949c32ba0a770709b22765025152681513
karajan <your-github-email@example.com>
2023-10-18 08:44:42 +0200

    added poetry for requirements management and addition logs in bootstrap-models

--- app/setup.sh
+++ app/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install llama-cpp-python with CUBAS ON to allow gpu offloading in local
-LLAMA_CUBLAS=1  pip install llama-cpp-python==0.1.65  --force-reinstall --verbose
+LLAMA_CUBLAS=1  pip install llama-cpp-python --force-reinstall --verbose
 
 # Install experimental cpp guidance package
 pip install git+https://github.com/Maximilian-Winter/guidance.git@313c726265c94523375b0dadd8954d19c01e709b
@@ -9,5 +9,20 @@ pip install git+https://github.com/Maximilian-Winter/guidance.git@313c726265c945
 # Install andromeda-chain with no dependencies
 pip install andromeda-chain==0.2.0 --no-deps
 
-# Install packages from requirements_local.txt
-pip install -r requirements_local.txt
+# Check if poetry is installed
+if ! command -v poetry &> /dev/null; then
+    echo "Poetry is not installed. Installing now..."
+    
+    # Installing poetry (this is the recommended way to install poetry)
+    curl -sSL https://install.python-poetry.org | python3
+else
+    echo "Poetry is already installed."
+fi
+
+# Navigate to the directory containing pyproject.toml if not already there
+# cd /path/to/directory/with/pyproject.toml  # Uncomment and replace with the correct path if needed
+
+# Install dependencies from pyproject.toml using poetry
+poetry install
+
+echo "Installation completed!"
